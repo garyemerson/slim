82f82982c045065706ed24f24646a59a50994b05
garyemerson <gmohammadioun@gmail.com>
2017-03-07 22:32:12 -0800

    save settings per view

--- slim.py
+++ slim.py
@@ -1,14 +1,7 @@
 import sublime
 import sublime_plugin
 
-toggled = False
-line = None
-gutter = None
-menu = None
-sidebar = None
-tabs = None
-status = None
-minimap = None
+view_settings = {}
 
 class barCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -23,55 +16,42 @@ class barCommand(sublime_plugin.TextCommand):
 
 class fooCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        global toggled
-        if toggled:
+        if self.view.id() in view_settings and view_settings[self.view.id()]["toggled"]:
             self.untoggle()
-            toggled = False
         else:
             self.toggle()
-            toggled = True
 
     def toggle(self):
-        global line
-        global gutter
-        global menu
-        global sidebar
-        global tabs
-        global status
-        global status
-        global minimap
+        if not self.view.id() in view_settings:
+            view_settings[self.view.id()] = {"toggled": False, "line": None, "gutter": None, "menu": None, "sidebar": None, "tabs": None, "status": None, "minimap": None}
 
-        line = self.view.settings().get("line_numbers")
+        curr_view_settings = view_settings[self.view.id()]
+
+        curr_view_settings["toggled"] = True
+        curr_view_settings["line"] = self.view.settings().get("line_numbers")
         self.view.settings().set("line_numbers", False)
-        gutter = self.view.settings().get("gutter")
+        curr_view_settings["gutter"] = self.view.settings().get("gutter")
         self.view.settings().set("gutter", False)
 
-        menu = self.view.window().is_menu_visible()
+        curr_view_settings["menu"] = self.view.window().is_menu_visible()
         self.view.window().set_menu_visible(False)
-        sidebar = self.view.window().is_sidebar_visible()
+        curr_view_settings["sidebar"] = self.view.window().is_sidebar_visible()
         self.view.window().set_sidebar_visible(False)
-        tabs = self.view.window().get_tabs_visible()
+        curr_view_settings["tabs"] = self.view.window().get_tabs_visible()
         self.view.window().set_tabs_visible(False)
-        status = self.view.window().is_status_bar_visible()
+        curr_view_settings["status"] = self.view.window().is_status_bar_visible()
         self.view.window().set_status_bar_visible(False)
-        minimap = self.view.window().is_minimap_visible()
+        curr_view_settings["minimap"] = self.view.window().is_minimap_visible()
         self.view.window().set_minimap_visible(False)
 
     def untoggle(self):
-        global line
-        global gutter
-        global menu
-        global sidebar
-        global tabs
-        global status
-        global status
-        global minimap
+        curr_view_settings = view_settings[self.view.id()]
+        self.view.settings().set("line_numbers", curr_view_settings["line"])
+        self.view.settings().set("gutter", curr_view_settings["gutter"])
 
-        self.view.settings().set("line_numbers", line)
-        self.view.settings().set("gutter", gutter)
-
-        self.view.window().set_menu_visible(menu)
-        self.view.window().set_sidebar_visible(sidebar)
-        self.view.window().set_tabs_visible(tabs)
-        self.view.window().set_status_bar_visible(status)
-        self.view.window().set_minimap_visible(minimap)
+        self.view.window().set_menu_visible(curr_view_settings["menu"])
+        self.view.window().set_sidebar_visible(curr_view_settings["sidebar"])
+        self.view.window().set_tabs_visible(curr_view_settings["tabs"])
+        self.view.window().set_status_bar_visible(curr_view_settings["status"])
+        self.view.window().set_minimap_visible(curr_view_settings["minimap"])
+        curr_view_settings["toggled"] = False
