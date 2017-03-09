d4346dee41fd3e884bdb05b01e9acb44c0e88ae4
garyemerson <gmohammadioun@gmail.com>
2017-03-08 20:29:23 -0800

    split toggling, add focus command, add notes file
    Toggling split between view and window, both keeping track of their
    respective settings. Focus command slims both the current view and
    window. Notes file used for potential features, bug fixes and other
    TODO thoughts.

--- slim.py
+++ slim.py
@@ -2,8 +2,9 @@ import sublime
 import sublime_plugin
 
 view_settings = {}
+window_settings = {}
 
-class barCommand(sublime_plugin.TextCommand):
+class ToggleFullCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.settings().set("line_numbers", True)
         self.view.settings().set("gutter", True)
@@ -14,44 +15,68 @@ class barCommand(sublime_plugin.TextCommand):
         self.view.window().set_status_bar_visible(True)
         self.view.window().set_minimap_visible(True)
 
-class fooCommand(sublime_plugin.TextCommand):
+class FocusCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if self.view.id() in view_settings and view_settings[self.view.id()]["toggled"]:
-            self.untoggle()
+        ToggleViewCommand(self.view).slim()
+        ToggleWindowCommand(self.view).slim()
+
+class ToggleViewCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if self.view.id() in view_settings and view_settings[self.view.id()]["slimmed"]:
+            self.unslim()
         else:
-            self.toggle()
+            self.slim()
 
-    def toggle(self):
+    def slim(self):
         if not self.view.id() in view_settings:
-            view_settings[self.view.id()] = {"toggled": False, "line": None, "gutter": None, "menu": None, "sidebar": None, "tabs": None, "status": None, "minimap": None}
+            view_settings[self.view.id()] = {"slimmed": False, "line": None, "gutter": None}
+        curr_settings = view_settings[self.view.id()]
+        if curr_settings["slimmed"]:
+            return
 
-        curr_view_settings = view_settings[self.view.id()]
-
-        curr_view_settings["toggled"] = True
-        curr_view_settings["line"] = self.view.settings().get("line_numbers")
+        curr_settings["line"] = self.view.settings().get("line_numbers")
+        curr_settings["gutter"] = self.view.settings().get("gutter")
         self.view.settings().set("line_numbers", False)
-        curr_view_settings["gutter"] = self.view.settings().get("gutter")
         self.view.settings().set("gutter", False)
+        curr_settings["slimmed"] = True
 
-        curr_view_settings["menu"] = self.view.window().is_menu_visible()
+    def unslim(self):
+        curr_settings = view_settings[self.view.id()]
+        self.view.settings().set("line_numbers", curr_settings["line"])
+        self.view.settings().set("gutter", curr_settings["gutter"])
+        curr_settings["slimmed"] = False
+
+class ToggleWindowCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        if self.view.window().id() in window_settings and window_settings[self.view.window().id()]["slimmed"]:
+            self.unslim()
+        else:
+            self.slim()
+
+    def slim(self):
+        if not self.view.window().id() in window_settings:
+            window_settings[self.view.window().id()] = {"slimmed": False, "menu": None, "sidebar": None, "tabs": None, "status": None, "minimap": None}
+        curr_settings = window_settings[self.view.window().id()]
+        if curr_settings["slimmed"]:
+            return
+
+        curr_settings["menu"] = self.view.window().is_menu_visible()
+        curr_settings["sidebar"] = self.view.window().is_sidebar_visible()
+        curr_settings["tabs"] = self.view.window().get_tabs_visible()
+        curr_settings["status"] = self.view.window().is_status_bar_visible()
+        curr_settings["minimap"] = self.view.window().is_minimap_visible()
         self.view.window().set_menu_visible(False)
-        curr_view_settings["sidebar"] = self.view.window().is_sidebar_visible()
         self.view.window().set_sidebar_visible(False)
-        curr_view_settings["tabs"] = self.view.window().get_tabs_visible()
         self.view.window().set_tabs_visible(False)
-        curr_view_settings["status"] = self.view.window().is_status_bar_visible()
         self.view.window().set_status_bar_visible(False)
-        curr_view_settings["minimap"] = self.view.window().is_minimap_visible()
         self.view.window().set_minimap_visible(False)
+        curr_settings["slimmed"] = True
 
-    def untoggle(self):
-        curr_view_settings = view_settings[self.view.id()]
-        self.view.settings().set("line_numbers", curr_view_settings["line"])
-        self.view.settings().set("gutter", curr_view_settings["gutter"])
-
-        self.view.window().set_menu_visible(curr_view_settings["menu"])
-        self.view.window().set_sidebar_visible(curr_view_settings["sidebar"])
-        self.view.window().set_tabs_visible(curr_view_settings["tabs"])
-        self.view.window().set_status_bar_visible(curr_view_settings["status"])
-        self.view.window().set_minimap_visible(curr_view_settings["minimap"])
-        curr_view_settings["toggled"] = False
+    def unslim(self):
+        curr_settings = window_settings[self.view.window().id()]
+        self.view.window().set_menu_visible(curr_settings["menu"])
+        self.view.window().set_sidebar_visible(curr_settings["sidebar"])
+        self.view.window().set_tabs_visible(curr_settings["tabs"])
+        self.view.window().set_status_bar_visible(curr_settings["status"])
+        self.view.window().set_minimap_visible(curr_settings["minimap"])
+        curr_settings["slimmed"] = False
