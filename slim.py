69ee2d83cf5577cd16defe97e7c9c65e9e5a75f1
garyemerson <gmohammadioun@gmail.com>
2018-05-26 14:19:33 -0700

    disable logic to check if already slimmed/unslimmed

--- slim.py
+++ slim.py
@@ -12,11 +12,11 @@ class UnfocusCommand(sublime_plugin.TextCommand):
         try:
             ToggleViewCommand(self.view).unslim()
         except Exception as e:
-            print_debug(e)
+            print_debug("got exception: {}".format(e))
         try:
             ToggleWindowCommand(self.view).unslim()
         except Exception as e:
-            print_debug(e)
+            print_debug("got exception: {}".format(e))
 
 class FocusCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -35,22 +35,27 @@ class ToggleViewCommand(sublime_plugin.TextCommand):
     def slim(self):
         if not self.view.id() in view_settings:
             print_debug('encountered new view {}'.format(self.view.id()))
-            view_settings[self.view.id()] = {'slimmed': False, 'line': None, 'gutter': None}
+            view_settings[self.view.id()] = {'slimmed': False}
         curr_settings = view_settings[self.view.id()]
-        if curr_settings['slimmed']:
-            print_debug('view {} already slimmed, aborting'.format(self.view.id()))
-            return
+        # if curr_settings['slimmed']:
+        #     print_debug('view {} already slimmed, aborting'.format(self.view.id()))
+        #     return
 
-        curr_settings['line'] = self.view.settings().get('line_numbers')
-        curr_settings['gutter'] = self.view.settings().get('gutter')
         self.view.settings().set('line_numbers', False)
         self.view.settings().set('gutter', False)
         curr_settings['slimmed'] = True
 
     def unslim(self):
+        if not self.view.id() in view_settings:
+            print_debug('encountered new view {}'.format(self.view.id()))
+            view_settings[self.view.id()] = {'slimmed': False}
         curr_settings = view_settings[self.view.id()]
-        self.view.settings().set('line_numbers', curr_settings['line'])
-        self.view.settings().set('gutter', curr_settings['gutter'])
+        # if curr_settings['slimmed']:
+        #     print_debug('view {} already slimmed, aborting'.format(self.view.id()))
+        #     return
+        
+        self.view.settings().set('line_numbers', True)
+        self.view.settings().set('gutter', True)
         curr_settings['slimmed'] = False
 
 class ToggleWindowCommand(sublime_plugin.TextCommand):
@@ -65,29 +70,31 @@ class ToggleWindowCommand(sublime_plugin.TextCommand):
     def slim(self):
         if not self.view.window().id() in window_settings:
             print_debug('encountered new window {}'.format(self.view.window().id()))
-            window_settings[self.view.window().id()] = {'slimmed': False, 'menu': None, 'sidebar': None, 'tabs': None, 'status': None, 'minimap': None}
+            window_settings[self.view.window().id()] = {'slimmed': False}
         curr_settings = window_settings[self.view.window().id()]
-        if curr_settings['slimmed']:
-            print_debug('window {} already slimmed, aborting'.format(self.view.window().id()))
-            return
+        # if curr_settings['slimmed']:
+        #     print_debug('window {} already slimmed, aborting'.format(self.view.window().id()))
+        #     return
 
-        curr_settings['menu'] = self.view.window().is_menu_visible()
-        curr_settings['sidebar'] = self.view.window().is_sidebar_visible()
-        curr_settings['tabs'] = self.view.window().get_tabs_visible()
-        curr_settings['status'] = self.view.window().is_status_bar_visible()
-        curr_settings['minimap'] = self.view.window().is_minimap_visible()
         self.view.window().set_menu_visible(False)
-        self.view.window().set_sidebar_visible(False)
+        # self.view.window().set_sidebar_visible(False)
         self.view.window().set_tabs_visible(False)
         self.view.window().set_status_bar_visible(False)
         self.view.window().set_minimap_visible(False)
         curr_settings['slimmed'] = True
 
     def unslim(self):
+        if not self.view.window().id() in window_settings:
+            print_debug('encountered new window {}'.format(self.view.window().id()))
+            window_settings[self.view.window().id()] = {'slimmed': False}
         curr_settings = window_settings[self.view.window().id()]
-        self.view.window().set_menu_visible(curr_settings['menu'])
-        self.view.window().set_sidebar_visible(curr_settings['sidebar'])
-        self.view.window().set_tabs_visible(curr_settings['tabs'])
-        self.view.window().set_status_bar_visible(curr_settings['status'])
-        self.view.window().set_minimap_visible(curr_settings['minimap'])
+        # if not curr_settings['slimmed']:
+        #     print_debug('window {} already unslimmed, aborting'.format(self.view.window().id()))
+        #     return
+
+        self.view.window().set_menu_visible(True)
+        # self.view.window().set_sidebar_visible(True)
+        self.view.window().set_tabs_visible(True)
+        self.view.window().set_status_bar_visible(True)
+        self.view.window().set_minimap_visible(True)
         curr_settings['slimmed'] = False
